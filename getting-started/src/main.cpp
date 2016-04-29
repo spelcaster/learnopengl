de9393002eb3635d674c93cc25a84f00f57635b7
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-04-29 00:00:36 -0300

    Adding the exercise 3 from shaders section
    - Shader to output position as the custom_color to be used in fragment shader;
    - Added a prototype to the exercise 3 function and a call to this function
      in main;
    - Add source code to shader_exercise3;
    - Response: Negative values are clamped to a value of 0.0f (black) that
      explains the black side of the triangle;

--- getting-started/src/main.cpp
+++ getting-started/src/main.cpp
@@ -15,11 +15,13 @@ void event_handler (GLFWwindow*, int, int, int, int);
 void hello_triangle ();
 void shader_exercise1 ();
 void shader_exercise2 ();
+void shader_exercise3 ();
 
 int main () {
     //hello_triangle();
     //shader_exercise1();
-    shader_exercise2();
+    //shader_exercise2();
+    shader_exercise3();
     return 0;
 }
 
